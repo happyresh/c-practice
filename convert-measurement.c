1083ea77b803a67b6c4c53540fc7b71a5d364fcc
Recio Eliniel <elinielvrecio@iskolarngbayan.pup.edu.ph>
2023-11-05 23:24:07 +0800

    Added feature

--- convert-measurement.c
+++ convert-measurement.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-int main(){
-    //Initialization of the program convert measurement
+int main() {
+    // Initialization of the program to convert measurements
     float FEET, INCHES, CENTIMETERS, METERS;
 
-    //Ask the user for the measurement in feet
-    printf("Enter the measurement in feet: ");
+    // Introduction and user instructions
+    printf("Welcome to the Measurement Converter!\n");
+    printf("This program converts feet into inches, centimeters, and meters.\n");
+
+    // Ask the user for the measurement in feet
+    printf("Please enter the measurement in feet: ");
     scanf("%f", &FEET);
 
-    //Conversions 
+    // Conversions 
     INCHES = FEET * 12; // 1 foot = 12 inches
     CENTIMETERS = INCHES * 2.54; // 1 inch = 2.54 cm
     METERS = CENTIMETERS / 100; // 1 meter = 100 cm
 
-    // Output of the program convert measurement
-    printf("%.2f feet is equivalent to:\n", FEET);
+    // Output of the program to convert measurements
+    printf("\n%.2f feet is equivalent to:\n", FEET);
     printf("%.2f inches\n", INCHES);
     printf("%.2f centimeters\n", CENTIMETERS);
     printf("%.4f meters\n", METERS);
 
-return 0;
+    return 0;
 }
