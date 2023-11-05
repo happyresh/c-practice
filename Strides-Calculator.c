3fe0688400abb3276ace001eb50436e301f7567c
Recio Eliniel <elinielvrecio@iskolarngbayan.pup.edu.ph>
2023-11-05 22:21:54 +0800

    Added features

--- Strides-Calculator.c
+++ Strides-Calculator.c
@@ -2,38 +2,35 @@
 
 int main(){
     // Constant for the program STRIDES CALCULATOR
-    const float FEET_IN_MILE= 5280;
-    const float STRIDE_LENGTH_FEET= 2.5;
+    const float FEET_IN_MILE= 5280; // 1 mile = 5280 feet
+    const float STRIDE_LENGTH_FEET= 2.5; // Arnie's stride is 2.5 feet (Assuming)
 
     // Variables for the program STRIDES CALCULATOR
-    unsigned int FIRST_MINUTE_STRIDE, LAST_MINUTE_STRIDE, TOTAL_MINUTES_JOGGED;
-    float AVERAGE_STRIDES, AVERAGE_DISTANCE_PER_MINUTE, TOTAL_DISTANCE_FEET, TOTAL_DISTANCE_MILES;
+    char USER[50]; 
+    unsigned int FIRST_MINUTE_STRIDE, LAST_MINUTE_STRIDE;
+    float AVERAGE_STRIDES, TOTAL_DISTANCE_MILES;
+
+    //Greetings for the user of program
+    printf("Enter your name: "); // Added features to make the program universal
+    scanf("%s", USER);
+
+    printf("Good day, %s! Welcome to the Strides Calculator.\n", USER);
 
     // Ask the user to input the first minute and last minute strides
-    printf("Enter the number of strides in the first minute: ");
+    printf("Enter the number of strides you made in the first minute: ");
     scanf("%d", &FIRST_MINUTE_STRIDE);
 
-    printf("Enter the number of strides in the last minute: ");
+    printf("Enter the number of strides you made in the last minute: ");
     scanf("%d", &LAST_MINUTE_STRIDE);
 
-    // Ask the user to input the total minutes jogged 
-    printf("Enter the time for total minutes jogged: ");
-    scanf("%d", &TOTAL_MINUTES_JOGGED);
-
     // Formula for the average of strides taken
-    AVERAGE_STRIDES= (FIRST_MINUTE_STRIDE+LAST_MINUTE_STRIDE)/2.0;
-
-    // Formula for the average distance jogged in feet
-    AVERAGE_DISTANCE_PER_MINUTE= AVERAGE_STRIDES * STRIDE_LENGTH_FEET;
-
-    // Formula for the total distance in feet
-    TOTAL_DISTANCE_FEET= AVERAGE_DISTANCE_PER_MINUTE * TOTAL_MINUTES_JOGGED;
+    AVERAGE_STRIDES= (FIRST_MINUTE_STRIDE + LAST_MINUTE_STRIDE)/2.0;
 
     // Formula to convert the total distance in feet to miles 
-    TOTAL_DISTANCE_MILES= TOTAL_DISTANCE_FEET / FEET_IN_MILE;
+    TOTAL_DISTANCE_MILES= (AVERAGE_STRIDES*STRIDE_LENGTH_FEET)/ FEET_IN_MILE;
 
     // Output for the program STRIDES CALCULATOR
-    printf("The total distance jogged in miles is %.2f", TOTAL_DISTANCE_MILES);
+    printf("The total distance jogged in miles is %.6f. Great Job!", TOTAL_DISTANCE_MILES);
 
 return 0;    
 }
