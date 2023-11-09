6785afed9ec9adb11464e7a4000e401484173e41
Recio Eliniel <141330367+happyresh@users.noreply.github.com>
2023-11-09 14:23:38 +0800

    Update dinosaur.c

--- dinosaur.c
+++ dinosaur.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    // Declarations for the two dinosaurs and their respective lifespans
-    char ERIC_THE_DINOSAUR[50];
-    char ALFRED_THE_DINOSAUR[50];
-    float ERIC_YEARS_LIVED, ALFRED_YEARS_LIVED;
+    // Variables for the program Dinosaur
+    char DINOSAUR_NAME[50];
+    float MONTHS_LIVED, DAYS_LIVED, SECONDS_LIVED, YEARS_LIVED;
     
-    // Declarations for calculated lifespans in different units
-    float ERIC_MONTHS_LIVED, ERIC_DAYS_LIVED, ERIC_SECONDS_LIVED;
-    float ALFRED_MONTHS_LIVED, ALFRED_DAYS_LIVED, ALFRED_SECONDS_LIVED;
+    // Input for the dinosaur's name and years lived
+    printf("Enter the name of the dinosaur: ");
+    scanf("%s", DINOSAUR_NAME);
     
-    // Input for the first dinosaur's name and years lived
-    printf("Enter the name of the first dinosaur: ");
-    scanf("%s", ERIC_THE_DINOSAUR);
+    printf("Enter how many years the dinosaur lived: ");
+    scanf("%f", &YEARS_LIVED);
     
-    printf("Enter the number of years %s lived: ", ERIC_THE_DINOSAUR);
-    scanf("%f", &ERIC_YEARS_LIVED);
-    
-    // Input for the second dinosaur's name and years lived
-    printf("Enter the name of the second dinosaur: ");
-    scanf("%s", ALFRED_THE_DINOSAUR);
-    
-    printf("Enter the number of years %s lived: ", ALFRED_THE_DINOSAUR);
-    scanf("%f", &ALFRED_YEARS_LIVED);
     
     // Calculations for lifespans in different units
-    ERIC_MONTHS_LIVED = ERIC_YEARS_LIVED * 12;
-    ERIC_DAYS_LIVED = ERIC_YEARS_LIVED * 365.25;
-    ERIC_SECONDS_LIVED = ERIC_YEARS_LIVED * 365 * 24 * 60 * 60;
-    
-    ALFRED_MONTHS_LIVED = ALFRED_YEARS_LIVED * 12;
-    ALFRED_DAYS_LIVED = ALFRED_YEARS_LIVED * 365.25;
-    ALFRED_SECONDS_LIVED = ALFRED_YEARS_LIVED * 365.25 * 24 * 60 * 60;
+    MONTHS_LIVED = YEARS_LIVED * 12;
+    DAYS_LIVED = YEARS_LIVED * 365.25;
+    SECONDS_LIVED = YEARS_LIVED * 365 * 24 * 60 * 60;
     
     // Output the calculated lifespans of the dinosaur
-    printf("\nThe number of months that %s lived is %.2f months, and for %s is %.2f months\n", ERIC_THE_DINOSAUR, ERIC_MONTHS_LIVED, ALFRED_THE_DINOSAUR, ALFRED_MONTHS_LIVED);
-    printf("The number of days that %s lived is %.2f days, and for %s is %.2f days\n", ERIC_THE_DINOSAUR, ERIC_DAYS_LIVED, ALFRED_THE_DINOSAUR, ALFRED_DAYS_LIVED);
-    printf("The number of seconds that %s lived is %.2f seconds, and for %s is %.2f seconds\n", ERIC_THE_DINOSAUR, ERIC_SECONDS_LIVED, ALFRED_THE_DINOSAUR, ALFRED_SECONDS_LIVED);
-    
+    printf("\nThe number of months that %s lived is %.2f months\n", DINOSAUR_NAME, MONTHS_LIVED);
+    printf("\nThe number of months that %s lived is %.2f months\n", DINOSAUR_NAME, DAYS_LIVED);
+    printf("\nThe number of months that %s lived is %.2f months\n", DINOSAUR_NAME, SECOND_LIVED);
+    printf("\n--Thank you for using our calculator!--\n");
     return 0;
 }
