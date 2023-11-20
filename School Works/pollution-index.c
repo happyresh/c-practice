963b59e62f6cf603ce934d966ef12deca56cc04a
Recio Eliniel <elinielvrecio@iskolarngbayan.pup.edu.ph>
2023-11-20 12:42:44 +0800

    Added some features

--- School Works/pollution-index.c
+++ School Works/pollution-index.c
@@ -4,17 +4,20 @@ int main() {
     // Initialization of the program Pollution Index
     int pollutionIndex;
 
+    // Welcome to the Pollution Index Checker
+    printf("=== Welcome to the Pollution Index Checker ===\n");
+
     // Input for the program Pollution Index
     printf("Please the enter the Pollution Index: ");
     scanf("%d", &pollutionIndex);
 
     // Checking the pollution index using if-else statement
     if (pollutionIndex < 35) {
-        printf("The pollution is classified as PLEASANT.\n");
+        printf("The pollution level is classified as PLEASANT.\n");
     } else if (pollutionIndex >= 35 && pollutionIndex <= 60) {
-        printf("The pollution is classified as UNPLEASANT.\n");
+        printf("The pollution level is classified as UNPLEASANT.\n");
     } else {
-        printf("The pollution is classified as HAZARDOUS.\n");
+        printf("The pollution level is classified as HAZARDOUS.\n");
     }
     
 return 0;
